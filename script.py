dd7d72e91f1e8b40887095614f29d636d59b9a12
Kurush Dubash <dubash.kurush@gmail.com>
2017-08-22 07:28:17 +0000

    Updated versions, added more constants

--- script.py
+++ script.py
@@ -1,42 +1,38 @@
 #!/usr/bin/python
 from bittrex import bittrex
-from bittrex_utils import *
 from constants import *
-from database import DataBase
 
 api_key = "8a3428dc510440228172f81f7d31f5bf "
 api_secret = "63b790f5c5ef44c3978620bb1ecca13f"
 
-
-# # print all the first cell of all the rows
-# for row in cur.fetchall():
-#     print row[0]
-
-# db.close()
-
-DB = DataBase(host="104.236.177.161",    # your host, usually localhost
-                     user="kurush",         # your username
-                     passwd="=2@fcM6LT6Vg=Exe",  # your password
-                     db="crypto")        # name of the data base
 Exchange = bittrex.Bittrex(api_key, api_secret)
-MESSAGE = "message"
-RESULT = "result"
-SUCCESS = "success"
 
 def track_currency_value():
-	market_summaries = Exchange.get_market_summaries()[RESULT]
-	currencies = []
-	bitcoin_usd_value = float(0)
-	for currency in market_summaries:
-		if(currency["MarketName"].split("-")[0] == "BTC"):
-			currency_in_market = MarketSummary(currency);
-			currencies.append(currency_in_market)
+    market_summaries = Exchange.get_market_summaries()[RESULT]
+    currencies = []
+    bitcoin_usd_value = float(0)
+    for currency in market_summaries:
+        if(currency["MarketName"].split("-")[0] == "BTC"):
+            currency_in_market = MarketSummary(currency);
+            currencies.append(currency_in_market)
+            print '"' + currency["MarketName"] + '",'
+            
+        if(currency["MarketName"].split("-")[0] == "USDT" \
+            and currency["MarketName"].split("-")[1] == "BTC"):
+            bitcoin_usd_value = float(currency["Last"])
+    for item in currencies:
+        item.usd = bitcoin_usd_value * item.last
+    # DB.update(currencies)
 
-		if(currency["MarketName"].split("-")[0] == "USDT" \
-			and currency["MarketName"].split("-")[1] == "BTC"):
-			bitcoin_usd_value = float(currency["Last"])
-	for item in currencies:
-		item.usd = bitcoin_usd_value * item.last
-	DB.update(currencies)
+# track_currency_value()
+Exchange.get_deposit_history();
 
-track_currency_value()
+for item in Exchange.get_market_summaries()[RESULT]:
+	value = item["MarketName"].split("-")[1]
+	response = Exchange.get_deposit_address(value)
+	print response
+	if(response[RESULT]!=None):
+		print response[RESULT]["Currecny"] + response[RESULT]["Address"]
+	else:
+		print "Not found for : " + value
+
