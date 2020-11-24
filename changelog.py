dcda064e7af13d548907f97fb6a02f3a3ada8901
Kurush Dubash <dubash.kurush@gmail.com>
2020-11-23 17:27:22 -0800

    Update changelog.py

--- changelog.py
+++ changelog.py
@@ -1,8 +1,8 @@
 from database import DataBase 
 
-DB = DataBase(host="104.236.177.161",    # your host, usually localhost
+DB = DataBase(host="",    # your host, usually localhost
                      user="kurush",         # your username
-                     passwd="=2@fcM6LT6Vg=Exe",  # your password
+                     passwd="",  # your password
                      db="crypto")        # name of the data base
 
 QUERIES = []
